02db7ab0023bd954b4c1e38c2be2133af16cf00b
Mohd Raziuddin <mohdraziuddin555@gmail.com>
2025-08-29 16:16:21 +0530

    Update About.jsx

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -58,7 +58,7 @@ const About = () => {
               boxShadow: '0 0 2px #8245ec, 0 0 2px #8245ec, 0 0 40px #8245ec',
             }}
           >
-            DOWNLOAD CV
+            DOWNLOAD RESUME
           </a>
           
         </div>
